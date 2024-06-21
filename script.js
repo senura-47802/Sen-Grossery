d5a4b909b8f48fb62cef668c18c9bb4af58f9177
Senura Perera <88277850+senura-47802@users.noreply.github.com>
2024-06-21 11:58:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+
+var modal = document.getElementById("myModal3");
+var btn = document.getElementById("myBtn3");
+var span = document.getElementsByClassName("close3")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+
+}
+function myfunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
+
+var modal = document.getElementById("myModal2");
+var btn = document.getElementById("myBtn2");
+var span = document.getElementsByClassName("close2")[0];
+btn.onclick = function() {
+  modal.style.display = "block";
+span.onclick = function() {
+  modal.style.display = "none";
+}
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+}
